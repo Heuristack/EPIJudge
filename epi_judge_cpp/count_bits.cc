ddb970dbaed0aaa214406ee31f23932ddab3618f
Lance LI <heuristack@gmail.com>
2018-03-28 22:33:20 +0800

    Bootcamp: Primitive Types - Count Bits

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,11 +1,15 @@
 #include "test_framework/generic_test.h"
+#include <bitset>
+#include <limits>
 
-short CountBits(unsigned int x) {
-  // Implement this placeholder.
-  return 0;
+short CountBits(unsigned int x)
+{
+    std::bitset<std::numeric_limits<decltype(x)>::digits> s(x);
+    return s.count();
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "count_bits.tsv", &CountBits,
