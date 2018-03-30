3cd48a99fed9132e9ad579d04540fe4faab4095c
Lance LI <heuristack@gmail.com>
2018-03-30 11:17:01 +0800

    The 3-sum problem: [sort][hash table]

--- epi_judge_cpp/three_sum.cc
+++ epi_judge_cpp/three_sum.cc
@@ -1,11 +1,31 @@
+#include <algorithm>
 #include <vector>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 
 using std::vector;
 
-bool HasThreeSum(vector<int> A, int t) {
-  // Implement this placeholder.
-  return true;
+bool HasTwoSum(vector<int> A, int t)
+{
+  std::sort(begin(A),end(A));
+  int i = 0;
+  int j = A.size() - 1;
+  while (i <= j) {
+    int s = A[i] + A[j];
+    if (s < t) { i++; }
+    if (s > t) { j--; }
+    if (s == t) return true;
+  }
+  return false;
+}
+
+bool HasThreeSum(vector<int> A, int t)
+{
+  std::unordered_map<int,int> m;
+  for (auto e : A) {
+    m.emplace(e,t-e);
+  }
+  return std::any_of(begin(m), end(m), [A](auto e){ return HasTwoSum(A,e.second); });
 }
 
 int main(int argc, char* argv[]) {
